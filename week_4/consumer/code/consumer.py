f140bc470dfc0db3d06d435851257ea8a7b3a701
Swetha V Raj <swethavraj96@gmail.com>
2022-10-16 10:59:08 -0700

    Including consumer service

---
+++ week_4/consumer/code/consumer.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+import http.client
+import os
+import json
+
+API_HOST = os.environ.get("API_HOST")
+API_PORT = os.environ.get("API_PORT")
+API_ENDPOINT = os.environ.get("API_ENDPOINT")
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def recommend():
+    connection = http.client.HTTPConnection(API_HOST + ":" + API_PORT)
+    connection.request('GET', API_ENDPOINT)
+    response = connection.getresponse()
+    meal = json.loads(response.read().decode())
+    return render_template('index.html', meal=meal)
+
+
+app.run()
+
